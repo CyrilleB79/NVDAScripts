d4c06b4d0933ae7dbfa3aa574127bef29c6038fb
CyrilleB79 <cyrille.bougot2@laposte.net>
2019-02-27 22:12:33 +0100

    Add windowUtils.py and beepError.py scripts + list scripts in readme.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # NVDAScripts
-This repo gathers various scripts for NVDA screen reader that do not (yet?) need to be packaged as an addon.
+
+This repo gathers various scripts for NVDA screen reader that I have not (yet?) packaged as NVDA addon.
+This may include potential features in development, (to be included later in an addon), debug scripts, test scripts, etc.
+
+## Installation
+
+* For NVDA up to 2018.4.1, copy the script you want to install in the corresponding subfolder (appModules or globalPlugins) in the user config folder of NVDA.
+* For NVDA from 2019.1:
+    * In the Advanced category of the settings dialog, check the "Enable loading custom code from Developer Scratchpad directory" option.
+    * Press the "Open developer scratchpad directory" button
+    * copy the script you want to install in the corresponding subfolder (appModules or globalPlugins) of the scratchpad folder.
+
+
+## Scripts
+
+### globalPlugins/autoLangSwitch.py
+
+This script allows to cycles through speech automatic language detection modes off, language only and language+dialect with NVDA+shift+L shortcut.
+
+### globalPlugins/beepError.py
+
+This script allows NVDA to beep on error even in NVDA non-test versions.
+To activate or de-activate beep error feature, press NVDA+control+alt+B
+
+### globalPlugins/debugTool.py
+
+This script allows to enable the stack trace logging of the speech function when pressing NVDA+control+alt+S. You may modify this file to pathc another function.
+See all instructions in the file for details on usage.
+
+### globalPlugins/windowutil.py
+
+This debug script allows to get various information on the current navigator object or associated window. It is an improvement of NVDA developer guide example 3
+
+Usage:
+
+* NVDA+LeftArrow : Announce the navigator object's currently selected property.
+* NVDA+Shift+LeftArrow or NVDA+Shift+RightArrow: select previous or next property and announce it for the navigator object.
+
+The list of supported properties is the following:
+name, role, state, value, windowClassName, windowControlID, windowHandle, pythonClass, pythonClassMRO
+
+If youhave installed Speech history review and copying  sélectionné addon from Tyler Spivey and James Scholes, you may use it to copy and paste the announced property to review it;
+review via copy/paste is especially useful for pythonClassMRO since it may be long.
---
+++ globalPlugins/windowutil.py
@@ -0,0 +1,99 @@
+# -*- coding: UTF-8 -*-
+#Window util  scripts for NVDA
+#Copyright (C) 2019 Cyrille Bougot
+#This file is covered by the GNU General Public License.
+
+#This debug script allows to get various information on the current navigator object or associated window. It is an improvement of NVDA developer guide example 3
+#To use it, put it in the globalPlugins folder.
+
+#Usage:
+#NVDA+LeftArrow : Announce the navigator object's currently selected property.
+#NVDA+Shift+LeftArrow or NVDA+Shift+RightArrow: select previous or next property and announce it for the navigator object.
+#The list of supported properties is the following:
+#name, role, state, value, windowClassName, windowControlID, windowHandle, pythonClass, pythonClassMRO
+#If youhave installed Speech history review and copying  sélectionné addon from Tyler Spivey and James Scholes, you may use it to copy and paste the announced property to review it;
+#review via copy/paste is especially useful for pythonClassMRO since it may be long.
+
+
+import globalPluginHandler
+import ui
+import api
+import controlTypes
+
+def _createDicControlTypesConstantes(prefix):
+	dic = {}
+	attributes = dir(controlTypes)
+	for name in attributes:
+		if name.startswith(prefix):
+			dic[getattr(controlTypes, name)] = name[len(prefix):]
+	#from logHandler import log
+	#log.debug(dic)
+	return dic
+_DIC_ROLES = _createDicControlTypesConstantes('ROLE_')
+_DIC_STATES = _createDicControlTypesConstantes('STATE_')
+
+def getStateInfos(o):
+	info = sorted(o.states)
+	names = ', '.join([_DIC_STATES[i] for i in info])
+	info = unicode(names) + u' (' + unicode(info) + u')'
+	return info
+			
+	
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	
+	_INFO_TYPES = ['name',
+		('role', lambda o: _DIC_ROLES[o.role] + u' (' + unicode(o.role) + u')'),
+		('states', getStateInfos),
+		'value',
+		'windowClassName',
+		'windowControlID',
+		'windowHandle',
+		('pythonClass', lambda o: type(o)),
+		('pythonClassMRO', lambda o: str(type(o).mro()).replace('>, <', ',\r\n').replace('[<', '\r\n', 1).replace('>]',''))]
+	
+	def __init__(self):
+		globalPluginHandler.GlobalPlugin.__init__(self)
+		self.index = 0
+		
+	def script_announceObjectInfo(self, gesture):
+		self.announceCurrentInfo()
+	script_announceObjectInfo.__doc__=_("Announce current object property.")
+	script_announceObjectInfo.category=_("WindowsUtil")
+		
+	def script_nextObjectInfo(self, gesture):
+		self.index = (self.index + 1) % len(self._INFO_TYPES)
+		self.announceCurrentInfo()
+	script_nextObjectInfo.__doc__=_("Select next object property and announce it.")
+	script_nextObjectInfo.category=_("WindowsUtil")
+		
+	def script_priorObjectInfo(self, gesture):
+		self.index = (self.index - 1) % len(self._INFO_TYPES)
+		self.announceCurrentInfo()
+	script_priorObjectInfo.__doc__=_("Select prior object property and announce it.")
+	script_priorObjectInfo.category=_("WindowsUtil")	
+	
+	def announceCurrentInfo(self):
+		infoType = self._INFO_TYPES[self.index]
+		nav = api.getNavigatorObject()
+		if isinstance(infoType, str):
+			info = getattr(nav, infoType)
+		else:
+			infoType, fun = infoType
+			info = fun(nav)
+		ui.message(infoType + u': ' + unicode(info))
+		return
+		if infoType == 'role':
+			name = self._DIC_ROLES[info]
+			info = name + u' (' + unicode(info) + u')'
+		elif infoType == 'states':
+			info = sorted(info)
+			names = ', '.join([self._DIC_STATES[i] for i in info])
+			info = unicode(names) + u' (' + unicode(info) + u')'
+		ui.message(infoType + u': ' + unicode(info))
+		
+	
+	__gestures = {
+		"kb:NVDA+rightArrow": "announceObjectInfo",
+		"kb:NVDA+shift+leftArrow": "priorObjectInfo",
+		"kb:NVDA+shift+rightArrow": "nextObjectInfo",
+		}
