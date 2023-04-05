0d7fcaddd9921ef5f47d01288c5aca8b5c201dde
Cyrille Bougot <cyrille.bougot2@laposte.net>
2023-04-05 12:05:23 +0200

    The notepad script to read the whole line when pressing Enter now also works with Windows 11.

--- appModules/notepad.py
+++ appModules/notepad.py
@@ -19,20 +19,28 @@ except ModuleNotFoundError:
 	# For older NVDA versions, just import AppModule
 	from appModuleHandler import AppModule
 from scriptHandler import script
+import api
 import controlTypes
 from speech import speech
 import textInfos
+from logHandler import log
 
 
+log.debug('Loading notepad.py module')
 
 class AppModule(AppModule):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		log.debug('Initializing Notepad appModule')
 
 	@script(
 		gesture="kb:enter",
 	)
 	def script_newLine(self, gesture):
-		obj=api.getFocusObject()
-		if obj.role == controlTypes.Role.EDITABLETEXT:
+		log.debug('Calling newline script')
+		obj = api.getFocusObject()
+		if obj.role in [controlTypes.Role.EDITABLETEXT, controlTypes.Role.DOCUMENT]:
+			log.debug('Current object is editable text or document')
 			info=obj.makeTextInfo(textInfos.POSITION_CARET)
 			info.expand(textInfos.UNIT_LINE)
 			speech.speakTextInfo(info, unit=textInfos.UNIT_LINE, reason=controlTypes.OutputReason.CARET)
